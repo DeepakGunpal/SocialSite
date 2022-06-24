d09538edabd8deac5568711fbfd853ed84aca50a
Deepak <deepakgunpal0@gmail.com>
2022-06-24 23:43:23 +0530

    userSchema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true, trim: true },
     lastName: { type: String, required: true, trim: true },
+    gender: { type: String, required: true, trim: true, enum: ["Male", "Female", "LGBTQ", "Prefer not to say"] },//drop down list
     email: { type: String, required: true, unique: true, trim: true },
     phone: { type: Number, required: true, trim: true },
     password: { type: String, required: true, trim: true },
     profileImage: { type: String, trim: true },
     bio: { type: String, trim: true },
-    follower: { type: Number, default: 0, trim: true }
+    follower: { type: Number, default: 0, trim: true },
+    isDeleted: { type: Boolean, required: true, trim: true }
 
 }, { timestamps: true })
 
