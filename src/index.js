8f9f413596bd7a9ccb49a2cfd5310899e79134a7
Deepak <deepakgunpal0@gmail.com>
2022-06-25 10:58:11 +0530

    userSchema

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const route = require('./route/route')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const route = require('./route/route');
 const multer = require("multer");
 
-app.use(express.json())
+app.use(express.json());
 // app.use(express.urlencoded({extended:true})) //not needed
-app.use(multer().any())
+app.use(multer().any());
 
 mongoose.connect(`mongodb+srv://Swetarun:${process.env.DBPassword}@cluster0.ebg8a.mongodb.net/socialSite`, {
     useNewUrlParser: true
@@ -15,17 +15,17 @@ mongoose.connect(`mongodb+srv://Swetarun:${process.env.DBPassword}@cluster0.ebg8
     .then(() => console.log("MongoDb is connected"))
     .catch(err => console.log(err))
 
-app.use('/', route)
+app.use('/', route);
 
 app.all('*', function (req, res) {
-    throw new Error("Bad Request")
+    throw new Error("Bad Request");
 })
 
 app.use(function (e, req, res, next) {
-    if (e.message == "Bad Request")
-        return res.status(400).send({ error: e.message })
+    if (e.message == "Bad Request");
+        return res.status(400).send({ error: e.message });
 })
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+    console.log('Express app running on port ' + (process.env.PORT || 3000));
 });
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
-
+//CEO swati di
 const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true, trim: true },
     lastName: { type: String, required: true, trim: true },
+    userName: { type: String, required: true, unique: true, trim: true },
     gender: { type: String, required: true, trim: true, enum: ["Male", "Female", "LGBTQ", "Prefer not to say"] },//drop down list
     email: { type: String, required: true, unique: true, trim: true },
-    phone: { type: Number, required: true, trim: true },
+    phone: { type: Number, required: true, unique: true, trim: true },
     password: { type: String, required: true, trim: true },
     profileImage: { type: String },
+    postCount: { type: Number, default: 0 },
+    posts: { type: Array, default: [] },
     bio: { type: String, trim: true },
     noOfFollower: { type: Number, default: 0 },
     followers: { type: Array, default: [] },
     following: { type: Array, default: [] },
     isDeleted: { type: Boolean, required: true, trim: true },
+    isDeactivated: { type: Boolean, required: true, trim: true },
     deletedAt: { type: Date, default: null }
 
 }, { timestamps: true })
