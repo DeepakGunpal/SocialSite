ef9d5e3174895413f06ea6b3a781ea996d61cdde
Ankitg996 <ankitg996@gmail.com>
2022-07-11 22:21:20 +0530

    comment_Ankit

---
+++ src/controller.js/commentController.js
@@ -0,0 +1,32 @@
+const commentModel = require('../models/commentModel')
+const {isValidBody} = require('../utility/validation')
+
+
+
+const createComment = function(req, res){
+    try{
+        let userId = req.params.userId;
+        let postId = req.params.postId;
+        let data = req.body;
+
+        // check the body has data
+        if(!isValidBody(data)) return res.status(400).send({status: false, message:'Please fill mandatory fields.'})
+
+        // Db call for check user Id and Post Id
+        const userCheck = await findOne({_id: userId, idDeleted: false})
+        const postCheck = await findOne({_id: postId, idDeleted: false})
+
+
+        // Authorization
+        
+
+        // post details
+
+
+
+
+    }
+    catch(err){
+        res.status(500).send({status: false, message: err.message})
+    }
+}
--- src/models/commentModel.js
+++ src/models/commentModel.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const commentSchema = new mongoose.Schema({
     comment: {type: String, required: true, trim: true},
     userId: {type: ObjectId, ref:'user', required: true},
+    postId: {type: ObjectId, ref:'post', required: true},
+    isLiked: {type: Boolean, defauld: false, trim: true},
     isDeleted: {type: Boolean, default: false, trim: true},
     deletedAt: {type:Date, default: null}
 
