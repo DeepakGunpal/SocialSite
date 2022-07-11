5f023e6810986b21c09f2327430f12165ad5b6e2
Rubi Chauhan <rubichauhan39@gmail.com>
2022-07-11 19:30:06 +0530

    post api

--- src/controller.js/postController.js
+++ src/controller.js/postController.js
@@ -1,7 +1,51 @@
-// const createPost = async (req, res) => {
-//     try {
-//     }
-// } catch (error) {
-//     res.status(500).send({ status: false, message: error.message });
-//   }
-// };
+require("dotenv").config();
+const userModel = require("../models/userModel");
+const { uploadFile } = require("../utility/aws");
+const {  isValidBody } = require("../utility/validation");
+
+
+
+const createPost = async (req, res) => {
+  try {
+    let data = req.body;
+
+    // let {post, caption, comment} = data;
+
+    if (!isValidBody(data)) {
+        return res.status(400).send({ status: false, message: "post details required" })
+    }
+
+    if (!isValidBody(data.post)) {
+        return res.status(400).send({status: false,message: 'post is required'});
+    }
+
+    const user = await userModel.findById({ _id: usertId })
+    if (!user) {
+        return res.status(404).send({ status: false, message: "userId does not exist" })
+    }
+    // if (!isValidBody(caption)) {
+    //     return res.status(400).send({status: false,message: 'cation is required'});
+    // }
+    
+
+    //===============upload post image=========//
+
+    let files = req.files;
+    if (files && files.length > 0) {
+      let uploadPostImage = await uploadFile(files[0]); 
+      post = uploadPostImage;
+    } else {
+      return res
+        .status(400)
+        .send({ status: false, message: "Please upload image" });
+    }
+
+
+    const newPost = await postModel.create(data);
+    res.status(201).send({ status: true, message: 'Posted successfull', data: newPost })
+  } catch (error) {
+    res.status(500).send({ status: false, message: error.message })
+  }
+}
+
+module.exports = { createPost }
--- src/route/route.js
+++ src/route/route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 const { createUser, loginUser, getUser, updateUser, updatePassword, getRequests, acceptRequest,userDelete, following} = require('../controller.js/userController')
-
+const {createPost}= require('../controller.js/postController')
 
 //----------------------------------------------------FEATURE-1_USER API'S----------------------------------------------------------------------------------------//
 router.post("/register", createUser)
@@ -16,6 +16,7 @@ router.delete('/deleteUser/:userId', userDelete )  // for delete user
 router.put('/following/:userId', following)   // fpr update follower and following
 
 //----------------------------------------------------FEATURE-2_POST API'S----------------------------------------------------------------------------------------//
+router.post("/user/:userId/feed",createPost)
 router.put("/editPost",)
 
 module.exports = router;
